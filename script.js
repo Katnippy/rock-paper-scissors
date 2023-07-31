b5dedf27af62f1a1bedf48a34156eebaebc9e415
kat <kat@pop-os.localdomain>
2023-07-31 12:41:16 +0100

    Create counters for player and computer wins, and draws.

--- script.js
+++ script.js
@@ -5,19 +5,44 @@ const winningChoices = {
   'scissors': 'paper',
 };
 
+let drawScore = document.getElementById('draw-score');
+let playerScore = document.getElementById('player-score');
+let computerScore = document.getElementById('computer-score');
+
 function getComputerChoice() {
   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
   
   return computerChoice;
 }
 
+function updateDrawScore() {
+  let dS = Number(drawScore.textContent);
+  dS++;
+  drawScore.textContent = dS;
+}
+
+function updatePlayerScore() {
+  let pS = Number(playerScore.textContent);
+  pS++;
+  playerScore.textContent = pS;
+}
+
+function updateComputerScore() {
+  let cS = Number(computerScore.textContent);
+  cS++;
+  computerScore.textContent = cS;
+}
+
 function checkWinner(p, c) {
   if (p === c) {
     alert('It\'s a draw!');
+    updateDrawScore();
   } else if (winningChoices.hasOwnProperty(p) && winningChoices[p] === c) {
     alert('The player wins!');
+    updatePlayerScore();
   } else {
     alert('The computer wins!');
+    updateComputerScore();
   }
 }
 
